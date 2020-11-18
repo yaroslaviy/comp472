2922979d764d7264cfc901d23c3df61a69f573e3
Star-Shopping <hyder555@hotmail.com>
2020-11-17 19:59:27 -0500

    added 50 puzzle generator

--- a2/main.py
+++ a2/main.py
@@ -2,6 +2,7 @@ import numpy as np
 from Solver import Solver
 import sys
 import getopt
+import random
 
 goal_state = np.array([[1, 2, 3, 4],
                        [5, 6, 7, 0]])
@@ -18,3 +19,19 @@ def Greedy(init_state, goal_state, heuristic):
 
 if __name__ == "__main__":
     print(Greedy(init_state, goal_state, "default"))
+
+
+#Generate 50 random puzzles and write them into a text file by line
+my_file = open("my_50_puzzles.txt", "w")
+
+for i in range(50):
+    possible_numbers = [0,1,2,3,4,5,6,7]
+    random.shuffle(possible_numbers)
+    print(possible_numbers)
+    
+    for numb in possible_numbers:   #.write() only takes strings as argument, so we use str() to write integer as a string into the file.
+        my_file.write(str(numb))   
+        my_file.write(" ")
+    my_file.write("\n")             #creates a new line after each puzzle's contents are written in the file
+
+my_file.close()                     
