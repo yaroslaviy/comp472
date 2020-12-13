2c324293cc2f35b2acdbd817e9ff0aa5d66e1d11
yaroslaviy <yaselokeev@gmail.com>
2020-12-12 19:46:34 -0500

    vocab fixes

--- a3/bowNoSkl.py
+++ a3/bowNoSkl.py
@@ -1,16 +1,15 @@
 import pandas as pd
-import string, re
-import pprint
-from nltk.tokenize import word_tokenize
+import re
 from collections import Counter
+import numpy
 
 # Declare the training set dataframe provided of 400 instances
-dfTraining = pd.read_csv("../472a3/covid_training.tsv", sep="\t", nrows=400, header=0)
+dfTraining = pd.read_csv("./covid_training.tsv", sep="\t",
+                         nrows=400, header=0, encoding="ISO-8859-1")
 
 # Only select classification label-1
 #xdf2 = dfTraining[["tweet_id", "text", "q1_label"]]
 xdf2 = dfTraining[["text", "q1_label"]]
-display(xdf2)
 
 # Make text columns into a list of tweets
 tweets = dfTraining["text"].tolist()
@@ -23,16 +22,37 @@ for i in tweets:
 # Remove spaces
 split_tweets = []
 for i in lower_case_tweets:
-    split_tweets.append(re.sub(r"\s+", " ", i)) # (r"[^a-zA-Z0-9]", " ", i) in case you want to ignore punctuation
-    
-# Tokenize tweet list
-vocabulary = []
-for i in split_tweets:
-    vocabulary.append(word_tokenize(i))
+    # (r"[^a-zA-Z0-9]", " ", i) in case you want to ignore punctuation
+    split_tweets.append(re.sub(r"\s+", " ", i))
 
-# 'Counter' counts the occurrences of each item in the list and returns a dictionary
-frequency_list = []
-for i in vocabulary:
-    frequency_list.append(dict(Counter(i)))
-#pprint.pprint(frequency_list) # this will print alphabetically
-display(frequency_list) # this will print as it is
+# Tokenize tweet list
+nested_voc = []
+for i in split_tweets:
+    nested_voc.append(i.split())
+
+vocabulary = [item for sublist in nested_voc for item in sublist]
+count_dict = Counter(vocabulary)
+
+print(count_dict)
+
+original_voc = []
+[original_voc.append(x) for x in vocabulary if x not in original_voc]
+
+filter_voc = numpy.copy(original_voc)
+filter_voc = filter_voc.tolist()
+
+for key, val in count_dict.items():
+    if(val == 1):
+        filter_voc.remove(key)
+
+
+print(len(filter_voc))
+print(len(original_voc))
+print(len(vocabulary))
+
+# # 'Counter' counts the occurrences of each item in the list and returns a dictionary
+# frequency_list = []
+# for i in vocabulary:
+#     frequency_list.append(dict(Counter(i)))
+# # pprint.pprint(frequency_list) # this will print alphabetically
+# print(frequency_list)  # this will print as it is
--- a3/bowSkl.py
+++ a3/bowSkl.py
@@ -3,7 +3,7 @@ from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Declare the training set dataframe provided of 400 instances
-dfTraining = pd.read_csv("../472a3/covid_training.tsv", sep="\t", nrows=400, header=0)
+dfTraining = pd.read_csv("./covid_training.tsv", sep="\t", nrows=400, header=0)
 
 # Only select classification label-1
 xdf2 = dfTraining[["tweet_id", "text", "q1_label"]]
@@ -14,12 +14,13 @@ tweets = dfTraining["text"].tolist()
 # Implementing Bag-Of-Words with sklearn
 vect = CountVectorizer()
 vect.fit(tweets)
-feat_names = vect.get_feature_names() # each word will be a column of the matrix
+# each word will be a column of the matrix
+feat_names = vect.get_feature_names()
 doc_array = vect.transform(tweets)
 doc_array.toarray()
-frequency_matrix = pd.DataFrame(doc_array.toarray(), columns = feat_names)
+frequency_matrix = pd.DataFrame(doc_array.toarray(), columns=feat_names)
 
-#pd.options.display.max_columns = None # BE CAREFUL: These 2 lines will remove the limits of the nb# of rows & columns that will display
-#pd.options.display.max_rows = None # so it may freeze you computer & take longer to display
+# pd.options.print.max_columns = None # BE CAREFUL: These 2 lines will remove the limits of the nb# of rows & columns that will print
+# pd.options.print.max_rows = None # so it may freeze you computer & take longer to print
 
-display(frequency_matrix)
+print(frequency_matrix)
